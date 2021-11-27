c5c0fa2c9b6691f4fd247de25fcfa6a3edf8bfc3
Ankita-Rawat <go2ankitarawat@gmail.com>
2021-11-28 01:17:26 +0530

    Initial Commit

---
+++ ApptizerApp/ApptizerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApptizerAppApp.swift
+//  ApptizerApp
+//
+//  Created by Bharat Singh on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ApptizerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
